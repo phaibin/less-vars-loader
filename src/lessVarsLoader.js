6ecd2b502f07489cb88c3203a9c5aa38cd644644
Leon <phaibin@gmail.com>
2021-03-26 13:45:29 +0800

    add default transform

--- src/lessVarsLoader.js
+++ src/lessVarsLoader.js
@@ -1,10 +1,11 @@
 import { getOptions } from 'loader-utils'
 import { loadLessWithImports, resolveLessVariables } from '@hon2a/less-vars-to-js'
 import fromPairs from 'lodash/fromPairs'
+import camelCase from 'lodash/camelCase'
 
 // @note Ignores source and reads it on its own, as `loadAndResolveLessVars` doesn't accept already read file.
 export async function lessVarsLoader() {
-  const { lessOptions, transform } = getOptions(this) ?? {}
+  const { lessOptions, transform } = getOptions(this) ?? (([key, value]) => [camelCase(key), /^\d+px$/.test(value) ? parseInt(value, 10) : value])
   const callback = this.async()
 
   const { code, imports } = loadLessWithImports(this.resourcePath)
